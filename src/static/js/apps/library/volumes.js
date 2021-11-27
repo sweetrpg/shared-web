7e7817e09051ac40cc7683f1265abe068eb23030
Paul Schifferer <paul@schifferers.net>
2021-11-26 18:55:18 -0800

    Wait for document ready.

--- src/static/js/apps/library/volumes.js
+++ src/static/js/apps/library/volumes.js
@@ -1,9 +1,11 @@
-$('div.col.volume').each(function (e) {
-    var id = e.id;
-    // var created = $(e).find("time#volume-" + id + "-created").attr("datetime");
-    var updated = $(e).find("time#volume-" + id + "-updated").attr("datetime");
-    // var createdBy = $(e).find("data#volume-" + id + "-created-by").attr("value");
-    // var updatedBy = $(e).find("data#volume-" + id + "-updated-by").attr("value");
-    var cell = $(e).find("#volume-" + id + "-date-info");
-    $(cell).html(moment(updated).fromNow());
+$(document).ready(function () {
+    $('div.col.volume').each(function (e) {
+        var id = e.id;
+        // var created = $(e).find("time#volume-" + id + "-created").attr("datetime");
+        var updated = $(e).find("time#volume-" + id + "-updated").attr("datetime");
+        // var createdBy = $(e).find("data#volume-" + id + "-created-by").attr("value");
+        // var updatedBy = $(e).find("data#volume-" + id + "-updated-by").attr("value");
+        var cell = $(e).find("#volume-" + id + "-date-info");
+        $(cell).html(moment(updated).fromNow());
+    });
 });
