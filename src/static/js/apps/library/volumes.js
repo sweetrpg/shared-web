960956404eddebc4a7dcdbbbc907babd0213d2f3
Paul Schifferer <paul@schifferers.net>
2022-02-06 13:34:55 -0800

    Add Vue

--- src/static/js/apps/library/volumes.js
+++ src/static/js/apps/library/volumes.js
@@ -1,11 +1,33 @@
 $(document).ready(function () {
-    $('div.col.volume').each(function (i, e) {
-        var id = e.id;
-        // var created = $(e).find("time#volume-" + id + "-created").attr("datetime");
-        var updated = $(e).find("time#volume-" + id + "-updated").attr("datetime");
-        // var createdBy = $(e).find("data#volume-" + id + "-created-by").attr("value");
-        // var updatedBy = $(e).find("data#volume-" + id + "-updated-by").attr("value");
-        var cell = $(e).find("#volume-" + id + "-date-info");
-        $(cell).html(moment(updated).fromNow());
-    });
+    // $('div.col.volume').each(function (i, e) {
+    //     var id = e.id;
+    //     // var created = $(e).find("time#volume-" + id + "-created").attr("datetime");
+    //     var updated = $(e).find("time#volume-" + id + "-updated").attr("datetime");
+    //     // var createdBy = $(e).find("data#volume-" + id + "-created-by").attr("value");
+    //     // var updatedBy = $(e).find("data#volume-" + id + "-updated-by").attr("value");
+    //     var cell = $(e).find("#volume-" + id + "-date-info");
+    //     $(cell).html(moment(updated).fromNow());
+    // });
+});
+
+var start = 0;
+var length = 20;
+
+function fetch_volumes(start, length) {
+    var api_url = $('data#api-base-url').val();
+    fetch(api_url + '/volumes/data?start=' + start + '&length=' + length)
+        .then((res) => res.json())
+        .then((data) => {
+            vue.volumes = data.items;
+        });
+}
+
+vue = Vue({
+    el: '#app',
+    data: {
+        'volumes': []
+    },
+    created() {
+        fetch_volumes(start, length);
+    }
 });
