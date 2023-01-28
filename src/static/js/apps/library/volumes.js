d76b7cb9278c360c9e2996c869cc8ee718758a3e
Paul Schifferer <paul@schifferers.net>
2023-01-28 10:25:46 -0800

    Update JS

--- src/static/js/apps/library/volumes.js
+++ src/static/js/apps/library/volumes.js
@@ -1,14 +1,3 @@
-$(document).ready(function () {
-    // $('div.col.volume').each(function (i, e) {
-    //     var id = e.id;
-    //     // var created = $(e).find("time#volume-" + id + "-created").attr("datetime");
-    //     var updated = $(e).find("time#volume-" + id + "-updated").attr("datetime");
-    //     // var createdBy = $(e).find("data#volume-" + id + "-created-by").attr("value");
-    //     // var updatedBy = $(e).find("data#volume-" + id + "-updated-by").attr("value");
-    //     var cell = $(e).find("#volume-" + id + "-date-info");
-    //     $(cell).html(moment(updated).fromNow());
-    // });
-});
 
 function fetch_volumes(start, length) {
     var api_url = $('data#api-base-url').val();
@@ -20,15 +9,28 @@ function fetch_volumes(start, length) {
         });
 }
 
-let vue = Vue({
-    el: '#app',
-    delimiters: ['[[', ']]'],
-    data: {
-        'volumes': [],
-        'startPage': 0,
-        'pageLength': 20,
-    },
-    created() {
-        fetch_volumes(this.startPage, this.pageLength);
-    }
-}).mount('#app');
+$(document).ready(function () {
+    // $('div.col.volume').each(function (i, e) {
+    //     var id = e.id;
+    //     // var created = $(e).find("time#volume-" + id + "-created").attr("datetime");
+    //     var updated = $(e).find("time#volume-" + id + "-updated").attr("datetime");
+    //     // var createdBy = $(e).find("data#volume-" + id + "-created-by").attr("value");
+    //     // var updatedBy = $(e).find("data#volume-" + id + "-updated-by").attr("value");
+    //     var cell = $(e).find("#volume-" + id + "-date-info");
+    //     $(cell).html(moment(updated).fromNow());
+    // });
+
+    let vue = Vue({
+        el: '#app',
+        delimiters: ['[[', ']]'],
+        data: {
+            'volumes': [],
+            'startPage': 0,
+            'pageLength': 20,
+        },
+        created() {
+            fetch_volumes(this.startPage, this.pageLength);
+        }
+    }).mount('#app');
+
+});
